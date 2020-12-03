af8647ddc5a88d18a7422ce94e305533397ab5eb
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-03 22:00:33 +0700

    add-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import { Cards, CountryPicker } from './components';
+
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>App</h1>
+                <Cards />
+                <CountryPicker />
             </div>
         )
     }
