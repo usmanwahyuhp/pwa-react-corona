b57d669cb061f51d4ff6e474367c8c47c5952252
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-03 22:30:19 +0700

    first-fetch-api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 import { Cards, CountryPicker } from './components';
+import { fetchData } from './api';
 import styles from './App.module.css';
 
 class App extends React.Component {
+
+    async componentDidMount(){
+        const data = await fetchData();
+
+        console.log(data);
+    }
+    
     render() {
         return (
             <div className={styles.container}>
