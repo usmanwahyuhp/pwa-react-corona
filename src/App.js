d77d9000ee74c769b5a54977b3e10aba343b7386
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-04 07:53:15 +0700

    bring data to component

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import { fetchData } from './api';
 import styles from './App.module.css';
 
 class App extends React.Component {
+    state = {
+        data: {},
+    }
 
     async componentDidMount(){
-        const data = await fetchData();
+        const fetchedData = await fetchData();
 
-        console.log(data);
+        this.setState({ data: fetchedData });
     }
     
     render() {
+        const { data } = this.state;
+
         return (
             <div className={styles.container}>
-                <Cards />
+                <Cards data={data} />
                 <CountryPicker />
             </div>
         )
