844ce33d2b3171528df2e485614054e0344eacc9
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-09 14:14:22 +0700

    Using changeurl for fetch specific country data

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ class App extends React.Component {
     }
 
     handleCountryChange = async (country) => {
-        console.log(country);
+        const fetchedData = await fetchData(country);
+
+        console.log(fetchedData);
     }
     
     render() {
