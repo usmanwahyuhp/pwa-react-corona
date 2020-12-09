ba3b0286e1bcdf00934382798b745638c8052882
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-09 16:38:12 +0700

    Using country data for barChart

--- src/App.js
+++ src/App.js
@@ -19,17 +19,17 @@ class App extends React.Component {
     handleCountryChange = async (country) => {
         const fetchedData = await fetchData(country);
 
-        console.log(fetchedData);
+        this.setState({ data: fetchedData, country: country });
     }
     
     render() {
-        const { data } = this.state;
+        const { data, country } = this.state;
 
         return (
             <div className={styles.container}>
                 <Cards data={data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange} />
-                <Chart />
+                <Chart data={data} country={country} />
             </div>
         )
     }
